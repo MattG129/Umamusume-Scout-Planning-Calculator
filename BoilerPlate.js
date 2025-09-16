2e8267938ce5ab284140f747b49652bba7a87a2c
MattG129 <61367937+MattG129@users.noreply.github.com>
2025-09-16 01:51:48 -0400

    Added a js file that will handle standardizing the header and footer.

---
+++ BoilerPlate.js
@@ -0,0 +1,28 @@
+$(function() {
+	$('#Header').html(`
+		<center><h1>Umamusume Scout Planning Calculator</h1></center>
+
+		<center>
+	    	<div class="alert alert-warning" role="alert" style="max-width: 80%;">
+		        Due to upcoming changes in rewards given from the Login Bonus, Daily Missions, Team Trials, and Club Rankings, the estimates provided by this calculator may vary drastically over the next few weeks.
+	    	</div>
+		</center>
+	`)
+});
+
+
+$(function() {
+	let Footer = '<center> <p>&#xA9; All rights reserved by Cygames, Inc. Other properties belong to their respective owners.</p>'
+
+	if (Page != 'index') {
+		Footer += '<a href="index.html" style="color: #054295; margin: 5px;"><i class="bi bi-arrow-left"></i> Back to calculator</a>'
+	};
+
+	if (Page != 'MoreInfo') {
+		Footer += '<a href="MoreInfo.html" style="color: #054295; margin: 5px;">More info</a>'
+	};
+	
+	Footer += '<a target="_blank" href="https://github.com/MattG129/Genshin-Wish-Calculator/issues" style="color: #054295; margin: 5px;">Report an issue</a> </center>'
+	
+	$('#Footer').html(Footer)
+});
