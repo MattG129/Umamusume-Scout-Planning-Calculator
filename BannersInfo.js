64fd93dc0de8b557cc6bca957cc7c6d466536dc9
MattG129 <61367937+MattG129@users.noreply.github.com>
2025-09-19 19:36:55 -0400

    Corrected an erroneous variable name.

--- BannersInfo.js
+++ BannersInfo.js
@@ -5,145 +5,145 @@ const BannerTypes = {
 
 const BannersInfo = [
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Haru Urara (New Year)',
         JPStartDate: moment('30 Dec 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('10 Jan 2022', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'TM Opera O (New Year)',
         JPStartDate: moment('30 Dec 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('10 Jan 2022', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Tamamo Cross',
         JPStartDate: moment('21 Dec 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('30 Dec 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Fine Motion',
         JPStartDate: moment('13 Dec 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('21 Dec 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Oguri Cap (Christmas)',
         JPStartDate: moment('28 Nov 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('13 Dec 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Biwa Hayahide (Christmas)',
         JPStartDate: moment('28 Nov 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('13 Dec 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Mejiro Dober',
         JPStartDate: moment('18 Nov 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('28 Nov 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Tosen Jordan',
         JPStartDate: moment('7 Nov 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('18 Nov 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Symboli Rudolf (Festival)',
         JPStartDate: moment('27 Oct 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('7 Nov 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Gold City (Festival)',
         JPStartDate: moment('27 Oct 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('7 Nov 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Manhattan Cafe',
         JPStartDate: moment('19 Oct 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('27 Oct 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Kawakami Princess',
         JPStartDate: moment('10 Oct 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('19 Oct 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Rice Shower (Halloween)',
         JPStartDate: moment('28 Sept 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('10 Oct 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Super Creek (Halloween)',
         JPStartDate: moment('28 Sept 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('10 Oct 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Agnes Digital',
         JPStartDate: moment('19 Sept 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('28 Sept 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Hishi Akebono',
         JPStartDate: moment('9 Sept 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('19 Sept 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Matikanefukukitaru (Full Armor)',
         JPStartDate: moment('29 Aug 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('9 Sept 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Eishin Flash',
         JPStartDate: moment('19 Aug 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('29 Aug 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Meisho Doto',
         JPStartDate: moment('10 Aug 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('19 Aug 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Special Week (Summer)',
         JPStartDate: moment('28 Jul 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('10 Aug 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Maruzensky (Summer)',
         JPStartDate: moment('28 Jul 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('10 Aug 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Gold City',
         JPStartDate: moment('19 Jul 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('28 Jul 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Fuji Kiseki',
         JPStartDate: moment('11 Jul 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('19 Jul 2021', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'El Condor Pasa (Fantasy)',
         JPStartDate: moment('28 Jun 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('11 Jul 2021', "DD MMM YYYY").toDate(),
@@ -152,7 +152,7 @@ const BannersInfo = [
         GlobalEndDate: moment('4 Oct 2025', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Grass Wonder (Fantasy)',
         JPStartDate: moment('28 Jun 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('11 Jul 2021', "DD MMM YYYY").toDate(),
@@ -161,7 +161,7 @@ const BannersInfo = [
         GlobalEndDate: moment('4 Oct 2025', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Hishi Amazon',
         JPStartDate: moment('20 Jun 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('28 Jun 2021', "DD MMM YYYY").toDate(),
@@ -170,7 +170,7 @@ const BannersInfo = [
         GlobalEndDate: moment('25 Sept 2025', "DD MMM YYYY").toDate()
     },
     {
-        TypeVar: BannerTypes['Uma'].Value,
+        Type: BannerTypes['Uma'].Value,
         Name: 'Seiun Sky',
         JPStartDate: moment('9 Jun 2021', "DD MMM YYYY").toDate(),
         JPEndDate: moment('20 Jun 2021', "DD MMM YYYY").toDate(),
