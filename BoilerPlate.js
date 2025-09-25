c3435afc6c28a1d4b6f8fb347d3c5d1379d9f25e
MattG129 <61367937+MattG129@users.noreply.github.com>
2025-09-24 21:31:12 -0400

    Code cleanup.

--- BoilerPlate.js
+++ BoilerPlate.js
@@ -7,12 +7,11 @@ $(function() {
 		        Due to upcoming changes in rewards given from Club Rankings, the estimates provided by this calculator may change at the beginning of October.
 	    	</div>
 		</center>
-	`)
+	`);
 });
 
-
 $(function() {
-	let Footer = '<center> <p>&#xA9; All rights reserved by Cygames, Inc. Other properties belong to their respective owners.</p>'
+	let Footer = '<center> <p>&#xA9; All rights reserved by Cygames, Inc. Other properties belong to their respective owners.</p>';
 
 	if (Page != 'index') {
 		Footer += '<a href="index.html" style="color: #054295; margin: 5px;"><i class="bi bi-arrow-left"></i> Back to calculator</a>'
@@ -21,10 +20,10 @@ $(function() {
 	if (Page != 'MoreInfo') {
 		Footer += '<a href="MoreInfo.html" style="color: #054295; margin: 5px;">More Info</a>'
 	};
-	
-	Footer += '<a target="_blank" href="https://github.com/MattG129/Umamusume-Wish-Planning-Calculator/issues" style="color: #054295; margin: 5px;">Feedback</a>'
-	
-	Footer += '<a target="_blank" href="https://mattg129.github.io/Genshin-Wish-Calculator/" style="color: #054295; margin: 5px;">Genshin Calculator</a> </center>'
-	
-	$('#Footer').html(Footer)
+
+	Footer += '<a target="_blank" href="https://github.com/MattG129/Umamusume-Wish-Planning-Calculator/issues" style="color: #054295; margin: 5px;">Feedback</a>';
+
+	Footer += '<a target="_blank" href="https://mattg129.github.io/Genshin-Wish-Calculator/" style="color: #054295; margin: 5px;">Genshin Calculator</a> </center>';
+
+	$('#Footer').html(Footer);
 });
